157e2b2cdced085c63fd8270acf41e990f915cf2
associaasppibra <associaasppibra@gmail.com>
2025-11-13 20:46:34 +0000

    feat: update code snippet with Solidity contract

--- components/landing/Ecosystem.web.tsx
+++ components/landing/Ecosystem.web.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, useWindowDimensions, TouchableOpacity, Platform } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, useWindowDimensions, TouchableOpacity, Platform } from 'react-native';
 import { Image as ExpoImage } from 'expo-image';
 
 const theme = {
@@ -16,15 +16,8 @@ const theme = {
   codeBorder: '#30363D',
 };
 
-const EcosystemCard = ({ style, cardColor, title, text, image, isDark, children }) => (
+const EcosystemCard = ({ style, cardColor, title, text, isDark, children }) => (
   <View style={[styles.card, { backgroundColor: cardColor }, style]}>
-    {image && (
-      <Image
-        source={{ uri: image }}
-        style={styles.cardImage}
-        resizeMode="cover"
-      />
-    )}
     <Text style={[styles.cardTitle, isDark && { color: theme.card.text }]}>
       {title}
     </Text>
@@ -42,11 +35,243 @@ const Ecosystem = () => {
   const { width } = useWindowDimensions();
   const isDesktop = width > 992;
 
+  const newCode = `/**
+ *Submitted for verification at BscScan.com on 2021-08-04
+*/
+
+/**
+
+ * Web: https://asppibra.com.br
+ *
+ *
+ */
+pragma solidity 0.5.16;
+
+interface IBEP20 {
+  function totalSupply() external view returns (uint256);
+  function decimals() external view returns (uint8);
+  function symbol() external view returns (string memory);
+  function name() external view returns (string memory);
+  function getOwner() external view returns (address);
+  function balanceOf(address account) external view returns (uint256);
+  function transfer(address recipient, uint256 amount) external returns (bool);
+  function allowance(address _owner, address spender) external view returns (uint256);
+  function approve(address spender, uint256 amount) external returns (bool);
+  function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract Context {
+  constructor () internal { }
+
+  function _msgSender() internal view returns (address payable) {
+    return msg.sender;
+  }
+
+  function _msgData() internal view returns (bytes memory) {
+    this;
+    return msg.data;
+  }
+}
+
+library SafeMath {
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    require(c >= a, "SafeMath: addition overflow");
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    return sub(a, b, "SafeMath: subtraction overflow");
+  }
+
+  function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+    require(b <= a, errorMessage);
+    uint256 c = a - b;
+    return c;
+  }
+
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    require(c / a == b, "SafeMath: multiplication overflow");
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    return div(a, b, "SafeMath: division by zero");
+  }
+
+  function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+    require(b > 0, errorMessage);
+    uint256 c = a / b;
+    return c;
+  }
+
+  function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+    return mod(a, b, "SafeMath: modulo by zero");
+  }
+
+  function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+    require(b != 0, errorMessage);
+    return a % b;
+  }
+}
+
+contract Ownable is Context {
+  address private _owner;
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+  constructor () internal {
+    address msgSender = _msgSender();
+    _owner = msgSender;
+    emit OwnershipTransferred(address(0), msgSender);
+  }
+
+  function owner() public view returns (address) {
+    return _owner;
+  }
+
+  modifier onlyOwner() {
+    require(_owner == _msgSender(), "Ownable: caller is not the owner");
+    _;
+  }
+
+  function renounceOwnership() public onlyOwner {
+    emit OwnershipTransferred(_owner, address(0));
+    _owner = address(0);
+  }
+
+  function transferOwnership(address newOwner) public onlyOwner {
+    _transferOwnership(newOwner);
+  }
+
+  function _transferOwnership(address newOwner) internal {
+    require(newOwner != address(0), "Ownable: new owner is the zero address");
+    emit OwnershipTransferred(_owner, newOwner);
+    _owner = newOwner;
+  }
+}
+
+contract BEP20Token is Context, IBEP20, Ownable {
+  using SafeMath for uint256;
+  mapping (address => uint256) private _balances;
+  mapping (address => mapping (address => uint256)) private _allowances;
+  uint256 private _totalSupply;
+  uint8 private _decimals;
+  string private _symbol;
+  string private _name;
+
+  constructor() public {
+    _name = "ASPPIBRA";
+    _symbol = "ASPPBR";
+    _decimals = 18;
+    _totalSupply = 21000000 * 10 ** 18;
+    _balances[msg.sender] = _totalSupply;
+    emit Transfer(address(0), msg.sender, _totalSupply);
+  }
+
+  function getOwner() external view returns (address) {
+    return owner();
+  }
+
+  function decimals() external view returns (uint8) {
+    return _decimals;
+  }
+
+  function symbol() external view returns (string memory) {
+    return _symbol;
+  }
+
+  function name() external view returns (string memory) {
+    return _name;
+  }
+
+  function totalSupply() external view returns (uint256) {
+    return _totalSupply;
+  }
+
+  function balanceOf(address account) external view returns (uint256) {
+    return _balances[account];
+  }
+
+  function transfer(address recipient, uint256 amount) external returns (bool) {
+    _transfer(_msgSender(), recipient, amount);
+    return true;
+  }
+
+  function allowance(address owner, address spender) external view returns (uint256) {
+    return _allowances[owner][spender];
+  }
+
+  function approve(address spender, uint256 amount) external returns (bool) {
+    _approve(_msgSender(), spender, amount);
+    return true;
+  }
+
+  function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) {
+    _transfer(sender, recipient, amount);
+    _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "BEP20: transfer amount exceeds allowance"));
+    return true;
+  }
+
+  function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+    _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
+    return true;
+  }
+
+  function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+    _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "BEP20: decreased allowance below zero"));
+    return true;
+  }
+
+  function mint(uint256 amount) public onlyOwner returns (bool) {
+    _mint(_msgSender(), amount);
+    return true;
+  }
+
+  function _transfer(address sender, address recipient, uint256 amount) internal {
+    require(sender != address(0), "BEP20: transfer from the zero address");
+    require(recipient != address(0), "BEP20: transfer to the zero address");
+    _balances[sender] = _balances[sender].sub(amount, "BEP20: transfer amount exceeds balance");
+    _balances[recipient] = _balances[recipient].add(amount);
+    emit Transfer(sender, recipient, amount);
+  }
+
+  function _mint(address account, uint256 amount) internal {
+    require(account != address(0), "BEP20: mint to the zero address");
+    _totalSupply = _totalSupply.add(amount);
+    _balances[account] = _balances[account].add(amount);
+    emit Transfer(address(0), account, amount);
+  }
+
+  function _burn(address account, uint256 amount) internal {
+    require(account != address(0), "BEP20: burn from the zero address");
+    _balances[account] = _balances[account].sub(amount, "BEP20: burn amount exceeds balance");
+    _totalSupply = _totalSupply.sub(amount);
+    emit Transfer(account, address(0), amount);
+  }
+
+  function _approve(address owner, address spender, uint256 amount) internal {
+    require(owner != address(0), "BEP20: approve from the zero address");
+    require(spender != address(0), "BEP20: approve to the zero address");
+    _allowances[owner][spender] = amount;
+    emit Approval(owner, spender, amount);
+  }
+
+  function _burnFrom(address account, uint256 amount) internal {
+    _burn(account, amount);
+    _approve(account, _msgSender(), _allowances[account][_msgSender()].sub(amount, "BEP20: burn amount exceeds allowance"));
+  }
+}`;
+
   const handleCopy = () => {
     if (Platform.OS === 'web') {
-        const codeToCopy = `import { Evergan } from 'nexera-sdk';\n\nconst evergan = new Evergan({\n  mobileOptimized: true,\n  secureRelay: true\n});`;
         const textArea = document.createElement('textarea');
-        textArea.value = codeToCopy;
+        textArea.value = newCode;
         textArea.style.position = 'fixed';
         textArea.style.opacity = '0';
         document.body.appendChild(textArea);
@@ -77,39 +302,16 @@ const Ecosystem = () => {
             {/* Code Snippet Section */}
             <View style={styles.codeSnippetContainer}>
               <View style={styles.codeHeader}>
-                <Text style={styles.codeFileName}>example.js</Text>
+                <Text style={styles.codeFileName}>ASPPBR.sol</Text>
                 <TouchableOpacity onPress={handleCopy}>
                    <Text style={styles.copyButtonText}>[□]</Text>
                 </TouchableOpacity>
               </View>
-              <View style={styles.codeBody}>
+              <ScrollView style={styles.codeBody}>
                 <Text style={styles.codeText}>
-                  <Text style={{color: '#C994FF'}}>import</Text>
-                  <Text> {'{'} </Text>
-                  <Text style={{color: '#79B8FF'}}>Evergan</Text>
-                  <Text> {'}'} </Text>
-                  <Text style={{color: '#C994FF'}}>from</Text>
-                  <Text style={{color: '#FFAB70'}}> 'nexera-sdk'</Text>
-                  <Text>;</Text>
-                  { '\n\n'}
-                  <Text style={{color: '#8B949E'}}>{ '// Inicializa o módulo Evergan'}</Text>
-                  { '\n'}
-                  <Text style={{color: '#C994FF'}}>const</Text>
-                  <Text> evergan = </Text>
-                  <Text style={{color: '#FFD372'}}>new</Text>
-                  <Text style={{color: '#A5D6FF'}}> Evergan</Text>
-                  <Text>({'{'})</Text>
-                  { '\n'}
-                  <Text>  mobileOptimized: </Text>
-                  <Text style={{color: '#79B8FF'}}>true</Text>
-                  <Text>,</Text>
-                  { '\n'}
-                  <Text>  secureRelay: </Text>
-                  <Text style={{color: '#79B8FF'}}>true</Text>
-                  { '\n'}
-                  <Text>{'});'}</Text>
+                  {newCode}
                 </Text>
-              </View>
+              </ScrollView>
             </View>
           </EcosystemCard>
         </View>
@@ -147,7 +349,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     width: '100%',
     gap: 24,
-    alignItems: 'stretch', // Align items to have the same height
+    alignItems: 'stretch',
   },
   columnLayoutMobile: {
     flexDirection: 'column',
@@ -165,12 +367,7 @@ const styles = StyleSheet.create({
     height: 580,
     borderWidth: 1,
     borderColor: theme.border,
-    flexDirection: 'column', // Ensure content inside flows downwards
-  },
-  cardImage: {
-    height: 200,
-    borderRadius: 12,
-    marginBottom: 20,
+    flexDirection: 'column',
   },
   cardTitle: {
     fontSize: 20,
@@ -196,7 +393,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: theme.codeBorder,
     flex: 1,
-    flexDirection: 'column', // content should be in a column
+    flexDirection: 'column',
   },
   codeHeader: {
     flexDirection: 'row',
@@ -213,13 +410,12 @@ const styles = StyleSheet.create({
     fontSize: 12,
     color: '#8B949E',
   },
-copyButtonText: {
+  copyButtonText: {
       color: '#8B949E',
       fontSize: 18,
   },
   codeBody: {
     padding: 16,
-    flex: 1,
   },
   codeText: {
     fontFamily: 'monospace',
